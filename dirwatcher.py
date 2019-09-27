1c849a9045c43b06c8a57a67538d8aa407a7b07f
Unknown <keybladeschoosen1@gmail.com>
2019-09-27 13:28:32 -0400

    update dirwatcher.py
    added flags and keyboard functions for closing program and added arguments.

--- dirwatcher.py
+++ dirwatcher.py
@@ -10,67 +10,78 @@ the program.  Author tag (below) helps instructors keep track of who
 wrote what, when grading.
 """
 import argparse
+import sys
+import signal
+import logging
+import time
+import os
+
 
 __author__ = "Jacob Walker"
 
-# Imports go at the top of your file, after the module docstring.
-# One module per import line. These are for example only.
-import sys
-from math import pi
-
-# This statement will run once at module import.
-print("Executing module import, name is {}".format(__name__))
+logging.basicConfig(filename='test.log', level=logging.INFO,
+                    format='%(levelname)s:%(message)s')
 
 # declare a few constants
-MY_MODULE_PI = pi
-
-
-def second_helper_func(y):
-    """docstring as first line"""
-    print("inside second_helper() with y={}".format(y))
-    return MY_MODULE_PI
-
 
 def first_helper_func(x):
     """docstring as first line"""
     print("inside first_helper() with x={}".format(x))
-    return second_helper_func(x)
 
 
-def function_to_be_implemented():
-    """You can raise your own exceptions"""
-    raise NotImplementedError("TODO Get to work and write this!")
+def signal_handler(sig_num, frame):
+    """
+    This is a handler for SIGTERM and SIGINT. Other signals can be mapped here
+    as well (SIGHUP?) Basically it just sets a global flag, and main() will
+    exit it's loop if the signal is trapped.
+    :param sig_num: The integer signal number that was trapped from the OS.
+    :param frame: Not used
+    :return None
+    """
+    signame = dict((k, v) for v, k in reversed(sorted(signal.__dict__.items()))
+                   if v.startswith('SIG') and not v.startswith('SIG_'))
+    logging.warning('Received {}'.format(signame[sig_num]))
+    raise KeyboardInterrupt
 
 
 def create_parser():
     """Create an argument parser object"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--todir', help='destination directory for something')
+    parser.add_argument('directory', help='directory to monitor')
+    parser.add_argument('magic_word', help='The magic word/words to watch for')
+    parser.add_argument('-i',
+                        '--interval',
+                        help='Sets the interval in seconds to check the directory for magic words',
+                        default=1)
+    parser.add_argument('-x', '--extension', help='Sets teh type of file to watch for', default='.txt')
     return parser
 
 
 def main(args):
     """Main function is declared as standalone, for testability"""
-    # You can call this main() function even if it is imported by
-    # another program.
-    print("main args: {}".format(args))
     parser = create_parser()
-    if not args:
-        parser.print_usage()
-        sys.exit(1)
     parsed_args = parser.parse_args(args)
-    # get the first argument, do something cool
-    result = first_helper_func(parsed_args.todir)
-    # return 0 if everything is awesome, nonzero otherwise ...
-    print('result is {}'.format(result))
-    return 0
+    print(parsed_args)
+    print("Starting App")
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    while 1:
+        try:
+            print("main args: {}".format(args))
+        except Exception as e:
+            logging.exception(e)
+        time.sleep(10)
 
 
 if __name__ == '__main__':
-    """Docstring goes here"""
+    """Runs the main loop until an interrupt like control+c are input."""
+    print("My Pid is {}".format(os.getpid()))
     print("Command line arguments: {}".format(sys.argv))
-    # Invoke standalone main() with cmdline argument list.
-    # Program should return status==0 if no errors.
-    status = main(sys.argv[1:])
-    # return status code to OS.
-    sys.exit(status)
+    start_time = time.time()
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        print("\nExiting by user request.\n"
+              "Process ran for {} seconds".format(time.time() - start_time))
+        sys.exit(0)
