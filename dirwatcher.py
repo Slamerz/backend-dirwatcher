86f7d88154fbcae71c512c12f02d3212c80c8af3
Unknown <keybladeschoosen1@gmail.com>
2019-10-15 11:01:27 -0400

    reformat dirwatcher.py
    updated code to pep8 style guidelines

--- dirwatcher.py
+++ dirwatcher.py
@@ -4,9 +4,9 @@
 Module docstring: One line description of what your program does.
 
 There should be a blank line in between description above, and this
-more detailed description. In this section you should put any caveats, 
+more detailed description. In this section you should put any caveats,
 environment variable expectations, gotchas, and other notes about running
-the program.  Author tag (below) helps instructors keep track of who 
+the program.  Author tag (below) helps instructors keep track of who
 wrote what, when grading.
 """
 import argparse
@@ -32,9 +32,11 @@ logger.addHandler(log_stream)
 checked_files = {}
 
 
-def magic_word_finder(directory, magic_word):
+def magic_word_finder(directory, magic_word, extension):
+    """Goes through each file of a given type in the given directory
+    and searches for given text."""
     d = os.path.abspath(directory)
-    text_files = [f for f in os.listdir(d) if ".txt" in f]
+
     text_files = [f for f in os.listdir(d) if extension in f]
     for f in text_files:
         line_count = len(open(f).readlines())
@@ -50,14 +52,12 @@ def magic_word_finder(directory, magic_word):
 
 
 def search_file(f, magic_word):
-    logging.info("searching {} for instances of {}".format(f, magic_word))
+    """Checks each line of a given file and searches for a given string"""
     logger.info("searching {} for instances of {}".format(f, magic_word))
 
     with open(f) as doc:
         content = doc.readlines()
-        last_index = 0
         for i, line in enumerate(content):
-            last_index += 1
             if magic_word in line:
                 logger.info("Match found for {} found on line {} in {}"
                             .format(magic_word, i + 1, f))
@@ -86,9 +86,11 @@ def create_parser():
     parser.add_argument('magic_word', help='The magic word/words to watch for')
     parser.add_argument('-i',
                         '--interval',
-                        help='Sets the interval in seconds to check the directory for magic words',
+                        help='Sets the interval in seconds to check the '
+                             'directory for magic words',
                         default=1)
-    parser.add_argument('-x', '--extension', help='Sets the type of file to watch for', default='.txt')
+    parser.add_argument('-x', '--extension', help='Sets the type of file to '
+                                                  'watch for', default='.txt')
     return parser
 
 
@@ -103,7 +105,11 @@ def main(args):
 
     while 1:
         try:
-            magic_word_finder(os.path.join(os.getcwd(), parsed_args.directory), parsed_args.magic_word)
+            magic_word_finder(
+                os.path.join(os.getcwd(), parsed_args.directory),
+                parsed_args.magic_word,
+                parsed_args.extension
+            )
         except Exception as e:
             logging.exception(e)
         time.sleep(parsed_args.interval)
